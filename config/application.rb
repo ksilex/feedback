03bb04e1451c16090d42cfede84c1393561797ea
ksilex <negative074@gmail.com>
2021-07-02 14:04:48 +0300

    green

--- config/application.rb
+++ config/application.rb
@@ -35,7 +35,7 @@ module RailsBase
     config.action_mailer.default_options = { from: ENV.fetch("MAILER_SENDER_ADDRESS") }
 
     # Set URL options to be able to use url_for helpers
-    config.action_mailer.default_url_options = { host: ENV.fetch("HOST"), port: ENV.fetch("PORT") }
+    config.action_mailer.default_url_options = { host: ENV.fetch("HOST") }
 
     # Add HOST to allowed list
     config.hosts << ENV.fetch("HOST")
